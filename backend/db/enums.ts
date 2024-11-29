5fa38b6af6d893f41210049cf18909dcd9c4c9c3
Haxurn <samitesfaye726@gmail.com>
2024-11-29 08:56:33 +0300

    Deleted

--- backend/db/enums.ts
+++
@@ -1,4 +0,0 @@
-import { pgEnum } from 'drizzle-orm/pg-core';
-
-export const userRole = pgEnum('role', ['STUDENT', 'PARENT', 'TEACHER', 'REGISTRAR', 'ADMIN']);
-export const bloodType = pgEnum('blood_type', ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']);
--- backend/db/schema/admin.ts
+++
@@ -1,8 +0,0 @@
-// backend/db/schema/admin.ts
-
-import { pgTable, text } from 'drizzle-orm/pg-core';
-
-export const admin = pgTable("admin", {
-    username: text("username").primaryKey(),
-    password: text("password").notNull(),
-});
