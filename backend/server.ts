81032361e6333042737109b97d200cfd77b93460
Haxurn <samitesfaye726@gmail.com>
2024-12-01 12:18:21 +0300

    feat(server): add CSRF protection and fix TypeScript issues
    - Integrated CSRF protection using 'csurf' middleware.
    - Extended Express Request type to recognize csrfToken method.
    - Added CSRF token endpoint to send token to the client.
    - Installed missing type definitions for 'csurf' and resolved TypeScript type errors.

--- backend/server.ts
+++ backend/server.ts
@@ -2,28 +2,46 @@
 import express, { Request, Response } from 'express';
 import { checkConnection } from './config/dbConfig';
 import dotenv from 'dotenv';
-import  setupSwagger  from './config/swaggerConfig';
+import setupSwagger from './config/swaggerConfig';
 import authRoutes from './routes/authRoutes'; 
+import helmet from 'helmet';
+import csrf from 'csurf';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
+
+app.use(helmet());
+
+// Set up CSRF protection
+const csrfProtection = csrf({ cookie: true });
+app.use(csrfProtection); // Apply CSRF protection globally (or you can apply it only to specific routes)
+
+// Use JSON parsing middleware
 app.use(express.json());
 
+const port = process.env.PORT || 5000;
 
+// Set up Swagger for API documentation
 setupSwagger(app);
 
 app.get('/', (req: Request, res: Response) => {
     res.json({
         message: '👋 Hello from SchoolHub! 🎓'
-   });
+    });
 });
 
-app.use('/api/auth', authRoutes)
+// API routes
+app.use('/api/auth', authRoutes);
+
+
+app.get('/csrf-token', (req: Request, res: Response) => {
+    // Send CSRF token to client
+    res.json({ csrfToken: req.csrfToken() });
+});
 
 app.listen(port, () => {
-    console.log(`🚀 SchoolHub backend is running on  http://localhost:${port} 🖥️`);
-    checkConnection()
+    console.log(`🚀 SchoolHub backend is running on http://localhost:${port} 🖥️`);
+    checkConnection();
 });
