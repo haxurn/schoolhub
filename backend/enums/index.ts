d9f2716e2bf7c06e355babb37407e02d0720a3a2
Haxurn <samitesfaye726@gmail.com>
2024-12-16 07:39:11 +0300

    feat: Update index.ts to include BloodGroupEnum
    - Added import for BloodGroupEnum from student.enum.ts.
    - Updated the Enums export to include BloodGroupEnum.
    - Introduced bloodGroupToPrisma mapping for blood group values.

--- backend/enums/index.ts
+++ backend/enums/index.ts
@@ -1,5 +1,19 @@
-import { GenderEnum } from "./student.enum";
+// backend/enums/index.ts
+import { GenderEnum, BloodGroupEnum } from "./student.enum";  
 
 export const Enums = {
-  GenderEnum
+  GenderEnum,
+  BloodGroupEnum, 
 }
+
+
+export const bloodGroupToPrisma = {
+  "A+": "A_PLUS",
+  "A-": "A_MINUS",
+  "B+": "B_PLUS",
+  "B-": "B_MINUS",
+  "O+": "O_PLUS",
+  "O-": "O_MINUS",
+  "AB+": "AB_PLUS",
+  "AB-": "AB_MINUS",
+} as const;
