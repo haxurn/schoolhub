d6600131eb3960e9e20cf9f7827897fb0202c20a
Haxurn <samitesfaye726@gmail.com>
2024-12-16 07:39:22 +0300

    refactor: Rearrange student.enum.ts structure
    - Added a comment header for clarity.
    - Moved the definition of BloodGroupEnum to the top of the file.
    - Removed the previous definition of bloodGroupToPrisma as it has been moved to index.ts.

--- backend/enums/student.enum.ts
+++ backend/enums/student.enum.ts
@@ -1,18 +1,8 @@
+// backend/enums/student.enum.ts
 
 import { z } from "zod";
 
 
 export const GenderEnum = z.enum(["Male", "Female"]);
+
 export const BloodGroupEnum = z.enum(["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"]);
-
-export const bloodGroupToPrisma = {
-  "A+": "A_PLUS",
-  "A-": "A_MINUS",
-  "B+": "B_PLUS",
-  "B-": "B_MINUS",
-  "O+": "O_PLUS",
-  "O-": "O_MINUS",
-  "AB+": "AB_PLUS",
-  "AB-": "AB_MINUS",
-} as const;
-
