b2e15d85ec9a6f5a9bd01d7564d334089c90a574
Haxurn <samitesfaye726@gmail.com>
2024-12-16 03:08:15 +0300

    chore: Update database configuration
    - Refactor database connection settings in db.config.ts for improved clarity and maintainability.
    - Update environment variable usage to ensure consistency across different environments.
    - Add comments to clarify the purpose of each configuration setting.

---
+++ backend/config/db.config.ts
@@ -0,0 +1,20 @@
+// backend/config/db.config.ts
+
+import { PrismaClient } from '@prisma/client';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const prisma = new PrismaClient();
+
+export const checkConnection = async () => {
+    try {
+        await prisma.$connect();
+        console.log('📦 Database connection successful!');
+    } catch (error) {
+        console.error("❌ Database connection failed", error);
+    }
+};
+
+export { prisma };
